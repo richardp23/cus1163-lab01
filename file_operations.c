8bfea2afa2c0cf88bc2be386288ee1fcd85a39ce
ubuntu <richard.p23@icloud.com>
2025-09-14 23:06:44 -0400

    First iteration of read function

--- file_operations.c
+++ file_operations.c
@@ -16,7 +16,7 @@ int create_and_write_file(const char *filename, const char *content) {
 printf("Creating file with content '%s'.\n", content);
     // TODO: Open or create the file for writing using the open() system call.
     // TODO: Use flags O_CREAT | O_WRONLY | O_TRUNC and permissions 0644.
-    fd = open(filename, O_CREAT, O_WRONLY, O_TRUNC, 0644);
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
     // TODO: Check if open() failed (fd == -1). If so, print an error using perror and return -1.
    if (fd == -1) {
 	   perror("ERROR: Could not open file");
@@ -27,7 +27,7 @@ printf("Creating file with content '%s'.\n", content);
 printf("File descripter value: %i\n", fd);
     // TODO: Write the content to the file using the write() system call.
     // TODO: Use the length of 'content' as the size to write.
-    write(fd, content, strlen(content));
+    bytes_written = write(fd, content, strlen(content));
     // TODO: Check if write() failed (bytes_written == -1). If so, print an error using perror, close the file, and return -1.
 if (bytes_written == -1) {
 	perror("ERROR: Could not write to file");
@@ -50,7 +50,7 @@ int read_file_contents(const char *filename) {
     // TODO: Declare an integer 'fd' for the file descriptor.
     int fd;
     // TODO: Create a buffer array of size 1024 to store the file data.
-    char* buffer = (char*)calloc(1024, sizeof(char));
+    char buffer[1024];
     // TODO: Declare a variable 'bytes_read' of type ssize_t to store how many bytes are read.
 	ssize_t bytes_read;
     // TODO: Print a message showing which file is being read.
@@ -67,20 +67,22 @@ int read_file_contents(const char *filename) {
     // TODO: Print the file descriptor value.
     printf("File descriptor value: %i\n", fd);
     // TODO: Print a header for the file contents.
-printf("Starting file read:");
+printf("Starting file read:\n");
     // TODO: Read the file contents using the read() system call in a loop.
     // TODO: Use sizeof(buffer) - 1 for the buffer size.
     // TODO: Null-terminate the buffer after each read.
     // TODO: Print the contents of the buffer.
     // TODO: Continue reading until read() returns 0.
-
+	while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
+		write(STDOUT_FILENO, buffer, bytes_read); 
+	}
     // TODO: Check if read() failed (bytes_read == -1). If so, print an error using perror, close the file, and return -1.
 if (bytes_read== -1) {
 	perror("ERROR: Could not read file");
 	return -1;
 }
     // TODO: Print a footer for the end of the file.
-printf("File read complete.");
+printf("\nFile read complete.\n");
     // TODO: Close the file using close(fd).
     if (close(fd) < 0) {
     // TODO: Check if close() failed. If so, print an error using perror and return -1.
